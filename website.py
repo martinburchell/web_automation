f23bbaba9117763bcbf46ebecd3cbffbf3121985
Martin Burchell <martinburchell@spamcop.net>
2013-08-02 11:08:39 +0100

    Added method to check if website is up

--- website.py
+++ website.py
@@ -6,7 +6,7 @@ from retry import retry
 class Website(object):
     debug = False
 
-    def __init__(self, domain, login, password, logger=None):
+    def __init__(self, domain, login=None, password=None, logger=None):
         cookie_jar = cookielib.CookieJar()
         self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie_jar))
         self.insecure_domain = 'http://' + domain
@@ -25,3 +25,11 @@ class Website(object):
     @retry(urllib2.URLError, tries=4, delay=3, backoff=2)
     def send_request_with_retry(self, url, post_data):
         return self.opener.open(url, post_data)
+
+    def is_up(self):
+        try:
+            self.opener.open(self.insecure_domain)
+        except urllib2.URLError:
+            return False
+
+        return True
